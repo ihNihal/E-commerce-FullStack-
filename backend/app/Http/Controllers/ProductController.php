99a9f42b52c5d95eb669d5dcc9115c3217e6be61
Istiaque Hasan <istiaque98@gmail.com>
2024-02-12 17:57:43 +0600

    Initiate Backend' git push ; '

---
+++ backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+        return Product::all();
+    }
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name'=> 'required|max:255',
+            'description'=>'required',
+            'price'=> 'required|numeric',
+        ]);
+        $product = Product::create($validated);
+        return response()->json($product, 201);
+
+    }
+    public function show (Product $product){
+        return response()->json($product, 200);
+    }
+
+    public function update(Request $request){
+        $validated = request()->validate([
+            'name'=> 'required|max:255',
+            'description'=>'required',
+            'price'=> 'required|numeric',
+        ]);
+        $product = PRODUCT->update($validated);
+        return response()->json($product, 201);
+
+    }
+    public function destroy(Product $product){
+        $product->delete();
+        return response('Deleted', 200);
+    }
+}
